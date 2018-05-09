3198a14a413badc47c936515019d7dd7f62e2b0e
Doi90 <davidpw@student.unimelb.edu.au>
2018-05-09 11:27:03 +1000

    Add test statistic workflow for SSDM model

---
+++ scripts/test_statistics/test_statistics_SSDM.R
@@ -0,0 +1,57 @@
+######################################################
+######################################################
+###                                                ###
+###         SSDM TEST STATISTICS WORKFLOW          ###
+###                                                ###
+###    This script calculates the various test     ###
+### statistics used in this analysis for the SSDMs ###
+###                                                ###
+######################################################
+######################################################
+
+#################
+### Load Data ###
+#################
+
+## Observed / Testing Data
+
+command <- sprintf("read.csv('data/%1$s/y_%1$s_fold%2$s_test_spatial.csv')", 
+                   dataset_id,                          # Need to build command to read in
+                   fold_id)                             # specific files for this CV fold
+
+y_test <- eval(parse(text = command))                   # Evaluate command to read in data
+
+y_test <- y_test[ , -1]                                 # Remove rownames
+
+#################################
+### Calculate Test Statistics ###
+#################################
+
+## Load Data
+
+filename <- sprintf("outputs/predictions/%s_%s_fold%s.rds",
+                    model_id,
+                    dataset_id,
+                    fold_id)
+
+SSDM_pred <- readRDS(filename)
+
+## Calculate Test Statistics
+
+SSDM_ts <- test_statistic(observed = y_test,
+                          predictions = SSDM_pred)
+
+### Save To File
+
+filename <- sprintf("outputs/test_statistics/%s_%s_fold%s_ts.rds",
+                    model_id,
+                    dataset_id,
+                    fold_id)
+
+saveRDS(SSDM_ts,
+        filename)
+
+### Memory purge
+
+rm(c("SSDM_pred",
+     "SSDM_ts"))
