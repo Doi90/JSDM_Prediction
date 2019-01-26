fa80f15b4653676865cf055a022aef4036b059c4
Doi90 <davidpw@student.unimelb.edu.au>
2019-01-27 10:25:46 +1100

    Fix SSDM/SESAM likelihood

--- scripts/test_statistics/likelihood_SSDM.R
+++ scripts/test_statistics/likelihood_SSDM.R
@@ -38,7 +38,7 @@ filename <- sprintf("outputs/predictions/%s_%s_fold%s.rds",
                     dataset_id,
                     fold_id)
 
-readRDS(filename)
+SSDM_predictions <- readRDS(filename)
 
 ## SESAM
 
@@ -46,23 +46,25 @@ filename <- sprintf("outputs/predictions/SESAM_%s_fold%s.rds",
                     dataset_id,
                     fold_id)
 
-readRDS(filename)
+SESAM_predictions <- readRDS(filename)
 
 ################################
 ### Calculate Log-Likelihood ###
 ################################ 
 
+# SSDM 
+
 ## Vector to store likelihoods
 
-log_lik_vector <- vector(length = dim(pred_array)[1])
+SSDM_log_lik_vector <- vector(length = dim(SSDM_predictions)[1])
 
 ## Calculate likelihoods
 
-for(i in seq_len(dim(pred_array)[1])){
+for(i in seq_len(dim(SSDM_predictions)[1])){
   
   ## Get predicted species probabilities for site
   
-  pred_probs <- pred_array[i, , 1]
+  pred_probs <- SSDM_predictions[i, , 1]
   
   ## Get observed species state for site
   
@@ -70,28 +72,72 @@ for(i in seq_len(dim(pred_array)[1])){
   
   ## Calculate log likelihood for site
   
-  log_lik <- sum(dbinom(x = obs_spp,
+  log_lik <- sum(dbinom(x = unlist(obs_spp),
                         size = 1,
                         prob = pred_probs,
                         log = TRUE))
   
   ## Add value to vector
   
-  log_lik_vector[i] <- log_lik
+  SSDM_log_lik_vector[i] <- log_lik
   
 }
 
 ## Obtain single likelihood value for whole model
 
-log_likelihood_estimate <- sum(log_lik_vector,
-                               na.rm = TRUE)
+SSDM_log_likelihood_estimate <- sum(SSDM_log_lik_vector,
+                                    na.rm = TRUE)
 
 ## Save to file
 
-filename <- sprintf("outputs/likelihood/%s_%s_fold%s_likelihood.rds",
-                    model_id,
+filename <- sprintf("outputs/likelihood/SSDM_%s_fold%s_likelihood.rds",
                     dataset_id,
                     fold_id)
 
-saveRDS(log_likelihood_estimate,
+saveRDS(SSDM_log_likelihood_estimate,
+        filename)
+
+# SESAM
+
+## Vector to store likelihoods
+
+SESAM_log_lik_vector <- vector(length = dim(SESAM_predictions)[1])
+
+## Calculate likelihoods
+
+for(i in seq_len(dim(SESAM_predictions)[1])){
+  
+  ## Get predicted species probabilities for site
+  
+  pred_probs <- SESAM_predictions[i, ]
+  
+  ## Get observed species state for site
+  
+  obs_spp <- y[i, ]
+  
+  ## Calculate log likelihood for site
+  
+  log_lik <- sum(dbinom(x = unlist(obs_spp),
+                        size = 1,
+                        prob = unlist(pred_probs),
+                        log = TRUE))
+  
+  ## Add value to vector
+  
+  SESAM_log_lik_vector[i] <- log_lik
+  
+}
+
+## Obtain single likelihood value for whole model
+
+SESAM_log_likelihood_estimate <- sum(SESAM_log_lik_vector,
+                                     na.rm = TRUE)
+
+## Save to file
+
+filename <- sprintf("outputs/likelihood/SESAM_%s_fold%s_likelihood.rds",
+                    dataset_id,
+                    fold_id)
+
+saveRDS(SESAM_log_likelihood_estimate,
         filename)
