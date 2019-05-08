c206c9ac8aba9556704ed221feb926effd1e678e
Doi90 <davidpw@student.unimelb.edu.au>
2019-05-08 12:52:30 +1000

    SSDMs now calculate independent and joint likelihoods

--- scripts/test_statistics/likelihood_SSDM.R
+++ scripts/test_statistics/likelihood_SSDM.R
@@ -63,48 +63,142 @@ SSDM_predictions_prob <- readRDS(filename)
 
 # SSDM - probabilities
 
-## Vector to store likelihoods
+## Independent likelihood
+
+### Vector to store likelihoods
 
 SSDM_log_lik_vector <- vector(length = dim(SSDM_predictions_prob)[1])
 
-## Calculate likelihoods
+### Calculate likelihoods
 
 for(i in seq_len(dim(SSDM_predictions_prob)[1])){
   
-  ## Get predicted species probabilities for site
+  ### Get predicted species probabilities for site
   
   pred_probs <- SSDM_predictions_prob[i, , 1]
   
-  ## Get observed species state for site
+  ### Get observed species state for site
   
   obs_spp <- y[i, ]
   
-  ## Calculate log likelihood for site
+  ### Calculate log likelihood for site
   
   log_lik <- sum(dbinom(x = unlist(obs_spp),
                         size = 1,
                         prob = pred_probs,
                         log = TRUE))
   
-  ## Add value to vector
+  ### Add value to vector
   
   SSDM_log_lik_vector[i] <- log_lik
   
 }
 
-## Obtain single likelihood value for whole model
+# ## Obtain single likelihood value for whole model
+# 
+# SSDM_log_likelihood_estimate <- sum(SSDM_log_lik_vector,
+#                                     na.rm = TRUE)
 
-SSDM_log_likelihood_estimate <- sum(SSDM_log_lik_vector,
-                                    na.rm = TRUE)
+## #Save to file
 
-## Save to file
-
-filename <- sprintf("outputs/likelihood/SSDM_prob_%s_fold%s_likelihood.rds",
+filename <- sprintf("outputs/likelihood/SSDM_prob_%s_fold%s_independent_likelihood.rds",
                     dataset_id,
                     fold_id)
 
-saveRDS(SSDM_log_likelihood_estimate,
+saveRDS(SSDM_log_lik_vector,
         filename)
+
+## Joint likelihood
+
+### Vector to store likelihoods
+
+SSDM_log_lik_vector <- vector(length = dim(SSDM_predictions_prob)[1])
+
+### Calculate likelihoods
+
+for(i in seq_len(dim(SSDM_predictions_prob)[1])){
+  
+  ### Get predicted species probabilities for site
+  
+  pred_probs <- SSDM_predictions_prob[i, , 1]
+  
+  ### Get observed species state for site
+  
+  obs_spp <- y[i, ]
+  
+  ### Mu and Sigma
+  
+  mu <- qnorm(pred_probs)
+  
+  sigma <- diag(length(pred_probs))
+  
+  ### Define probability distribution thresholds
+  
+  #### lower / upper to limit integral of density for lielihood
+  
+  lower <- rep(-Inf, length(pred_probs))  # default vector of -Inf lower limits
+  upper <- rep(+Inf, length(pred_probs))  # default vector of +Inf upper limits
+  
+  for(k in seq_len(length(pred_probs))){  # set actual lower/upper limits based on known occurrence states
+    
+    if(obs_spp[k] == 0){     # if species is absent
+      
+      upper[k] <- 0            # species absent when z<0
+      
+    } 
+    
+    if(obs_spp[k] == 1){     # if species is present
+      
+      lower[k] <- 0            # species present when z>0
+      
+    } 
+    
+  }
+  
+  #### Prediction for species assemblage at site i using values from slice a
+  
+  likelihood_tmp <- pmvnorm(mean = mu,
+                            sigma = sigma,
+                            lower = lower,
+                            upper = upper)
+  
+  # If the default GenzBretz algorithm fails, redo with slower Miwa algorithm
+  
+  if(likelihood_tmp[1] == 0 & attr(likelihood_tmp, "error") == 0){
+    
+    likelihood_tmp <- pmvnorm(mean = mu,
+                              sigma = sigma,
+                              lower = lower,
+                              upper = upper,
+                              algorithm = "Miwa")
+    
+  }
+  
+  if(likelihood_tmp[1] != 0){
+    
+    likelihood <- likelihood_tmp[1]
+    
+  }
+  
+  if(likelihood_tmp[1] == 0){
+    
+    likelihood <- attr(likelihood_tmp, "error")
+    
+  }
+
+  SSDM_log_lik_vector[i] <- log(likelihood)
+  
+}
+
+## #Save to file
+
+filename <- sprintf("outputs/likelihood/SSDM_prob_%s_fold%s_joint_likelihood.rds",
+                    dataset_id,
+                    fold_id)
+
+saveRDS(SSDM_log_lik_vector,
+        filename)
+
 
 # # SSDM - binary
 # 
