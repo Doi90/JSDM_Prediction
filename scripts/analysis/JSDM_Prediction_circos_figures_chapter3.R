485b04e82b90ca6589d88680f7ef90f95998c861
Doi90 <davidpw@student.unimelb.edu.au>
2019-09-17 11:40:02 +1000

    First changes to circos plot script. Switching from circlize to proper circos

--- scripts/analysis/JSDM_Prediction_circos_figures_chapter3.R
+++ scripts/analysis/JSDM_Prediction_circos_figures_chapter3.R
@@ -1,6 +1,25 @@
-library(circlize)
+#####################
+### Load Packages ###
+#####################
 
+library(nlme)
+library(ggplot2)
+library(RColorBrewer)
+library(PassButter)
+library(psych)
+library(logitnorm)
+library(AICcmodavg)
+# library(circlize)
 
+##############################
+### Load Prediction Script ###
+##############################
+
+source("scripts/prediction/MEM_predict.R")
+
+##################################
+### Set combination parameters ###
+##################################
 
 model_order_binary <- c("SESAM",
                         "MPR",
@@ -148,161 +167,166 @@ subsets <- list(ts_0_1_H = c("Accuracy",
                                # "NLR",
                                # "DOR"))
 
+########################################################
+### Load MEMs and create big dataframe of all values ###
+########################################################
 
+#########################
+### Old circlize code ###
+#########################
 
-
-for(pred in unique(plot_df$pred_type)){
-  
-  ## Subset Data
-  
-  if(pred %in% binary_predictions){
-    
-    tmp_circos_df <- plot_df[plot_df$pred_type == pred &
-                               plot_df$ts %in% binary_ts, ]
-    
-  }
-  
-  if(pred %in% probability_predictions){
-    
-    tmp_circos_df <- plot_df[plot_df$pred_type == pred &
-                               plot_df$ts %in% prob_ts, ]
-    
-  }
-  
-  ## Reformat Data
-  
-  ### Blank matrix
-  
-  if(pred %in% binary_predictions){
-    
-    plot_mat <- matrix(NA,
-                       nrow = length(unique(tmp_circos_df$model)),
-                       ncol = sum(unlist(lapply(lapply(subsets, 
-                                                       function(x) x %in% binary_ts), 
-                                                sum))),
-                       dimnames = list(model_order_binary,
-                                       c(binary_ts[binary_ts %in% subsets[["ts_0_1_H"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_0_1_L_a"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_0_1_L_b"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_1_1"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_Inf_Inf_H"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_Inf_Inf_0"]]],
-                                         binary_ts[binary_ts %in% subsets[["ts_0_Inf_L"]]])))
-    
-  }
-  
-  if(pred %in% probability_predictions){
-    
-    plot_mat <- matrix(NA,
-                       nrow = length(unique(tmp_circos_df$model)),
-                       ncol = sum(unlist(lapply(lapply(subsets, 
-                                                       function(x) x %in% prob_ts), 
-                                                sum))),
-                       dimnames = list(model_order_prob,
-                                       c(prob_ts[prob_ts %in% subsets[["ts_0_1_H"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_0_1_L_a"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_0_1_L_b"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_1_1"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_Inf_Inf_H"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_Inf_Inf_0"]]],
-                                         prob_ts[prob_ts %in% subsets[["ts_0_Inf_L"]]])))
-    
-  }
-  
-  ### Fill with values
-  
-  for(model_id in rownames(plot_mat)){
-    
-    for(ts_id in colnames(plot_mat)){
-      
-      plot_mat[model_id, ts_id] <- tmp_circos_df[tmp_circos_df$model == model_id &
-                                                   tmp_circos_df$ts == ts_id, "mean_diff"]
-      
-    }
-  }
-  
-  ## Make Circos plot
-  
-  col_fun <- colorRamp2(c(-100, 0, 100), c("Red", "White", "Blue"))
-  
-  factors <- rep(c("ts_0_1_H", 
-                   "ts_0_1_L_a",
-                   "ts_0_1_L_b",
-                   "ts_1_1",
-                   "ts_Inf_Inf_H", 
-                   "ts_Inf_Inf_0",
-                   "ts_0_Inf_L"),
-               times = c(sum(subsets[["ts_0_1_H"]] %in% prob_ts), 
-                         sum(subsets[["ts_0_1_L_a"]] %in% prob_ts),
-                         sum(subsets[["ts_0_1_L_b"]] %in% prob_ts), 
-                         sum(subsets[["ts_1_1"]] %in% prob_ts), 
-                         sum(subsets[["ts_Inf_Inf_H"]] %in% prob_ts), 
-                         sum(subsets[["ts_Inf_Inf_0"]] %in% prob_ts), 
-                         sum(subsets[["ts_0_Inf_L"]] %in% prob_ts)))
-
-  
-  mat_list <- list(ts_0_1_H = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_H"]])],
-                   ts_0_1_L_a = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_L_a"]])],
-                   ts_0_1_L_b = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_L_b"]])],
-                   ts_1_1 = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_1_1"]])],
-                   ts_Inf_Inf_H = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_Inf_Inf_H"]])],
-                   ts_Inf_Inf_0 = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_Inf_Inf_0"]])],
-                   ts_0_Inf_L = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_Inf_L"]])])
-  
-  dend_list <- list(ts_0_1_H = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_H"]])))),
-                    ts_0_1_L_a = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_L_a"]])))),
-                    ts_0_1_L_b = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_L_b"]])))),
-                    ts_1_1 = as.dendrogram(hclust(dist(t(mat_list[["ts_1_1"]])))),
-                    ts_Inf_Inf_H = as.dendrogram(hclust(dist(t(mat_list[["ts_Inf_Inf_H"]])))),
-                    ts_Inf_Inf_0 = as.dendrogram(hclust(dist(t(mat_list[["ts_Inf_Inf_0"]])))),
-                    ts_0_Inf_L = as.dendrogram(hclust(dist(t(mat_list[["ts_0_Inf_L"]])))))
-  
-  filename <- sprintf("outputs/test_statistics/plots/circos_%s_chapter3.pdf",
-                      pred)
-  
-  pdf(filename,
-      width = 10,
-      height = 10)
-  
-  circos.par(cell.padding = c(0, 
-                              0, 
-                              0, 
-                              0), 
-             gap.degree = 10,
-             "track.height" = 0.8)
-  
-  circos.initialize(factors = factors,
-                    xlim = cbind(0, table(factors)))
-  
-  circos.track(ylim = c(0, 6), 
-               bg.border = NA, 
-               panel.fun = function(x, y){
-                 
-                 sector.index = CELL_META$sector.index
-                 m = mat_list[[sector.index]]
-                 dend = dend_list[[sector.index]]
-                 
-                 m2 = m[, order.dendrogram(dend)]
-                 col_mat = col_fun(m2)
-                 nr = nrow(m2)
-                 nc = ncol(m2)
-                 for(i in 1:nr) {
-                   circos.rect(1:nc - 1, rep(nr - i, nc), 
-                               1:nc, rep(nr - i + 1, nc), 
-                               border = col_mat[i, ], col = col_mat[i, ])
-                   
-                 }
-                 circos.axis(h = "top",
-                             major.tick = FALSE,
-                             labels = colnames(mat_list[[sector.index]]),
-                             labels.cex = 0.6,
-                             labels.facing = "bending.inside",
-                             labels.away.percentage = 0.3)
-                 circos.yaxis(labels = model_order_prob, 
-                              labels.cex = 0.6)
-               })
-  
-  circos.clear()
-  
-  dev.off()
-}
+# for(pred in unique(plot_df$pred_type)){
+#   
+#   ## Subset Data
+#   
+#   if(pred %in% binary_predictions){
+#     
+#     tmp_circos_df <- plot_df[plot_df$pred_type == pred &
+#                                plot_df$ts %in% binary_ts, ]
+#     
+#   }
+#   
+#   if(pred %in% probability_predictions){
+#     
+#     tmp_circos_df <- plot_df[plot_df$pred_type == pred &
+#                                plot_df$ts %in% prob_ts, ]
+#     
+#   }
+#   
+#   ## Reformat Data
+#   
+#   ### Blank matrix
+#   
+#   if(pred %in% binary_predictions){
+#     
+#     plot_mat <- matrix(NA,
+#                        nrow = length(unique(tmp_circos_df$model)),
+#                        ncol = sum(unlist(lapply(lapply(subsets, 
+#                                                        function(x) x %in% binary_ts), 
+#                                                 sum))),
+#                        dimnames = list(model_order_binary,
+#                                        c(binary_ts[binary_ts %in% subsets[["ts_0_1_H"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_0_1_L_a"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_0_1_L_b"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_1_1"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_Inf_Inf_H"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_Inf_Inf_0"]]],
+#                                          binary_ts[binary_ts %in% subsets[["ts_0_Inf_L"]]])))
+#     
+#   }
+#   
+#   if(pred %in% probability_predictions){
+#     
+#     plot_mat <- matrix(NA,
+#                        nrow = length(unique(tmp_circos_df$model)),
+#                        ncol = sum(unlist(lapply(lapply(subsets, 
+#                                                        function(x) x %in% prob_ts), 
+#                                                 sum))),
+#                        dimnames = list(model_order_prob,
+#                                        c(prob_ts[prob_ts %in% subsets[["ts_0_1_H"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_0_1_L_a"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_0_1_L_b"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_1_1"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_Inf_Inf_H"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_Inf_Inf_0"]]],
+#                                          prob_ts[prob_ts %in% subsets[["ts_0_Inf_L"]]])))
+#     
+#   }
+#   
+#   ### Fill with values
+#   
+#   for(model_id in rownames(plot_mat)){
+#     
+#     for(ts_id in colnames(plot_mat)){
+#       
+#       plot_mat[model_id, ts_id] <- tmp_circos_df[tmp_circos_df$model == model_id &
+#                                                    tmp_circos_df$ts == ts_id, "mean_diff"]
+#       
+#     }
+#   }
+#   
+#   ## Make Circos plot
+#   
+#   col_fun <- colorRamp2(c(-100, 0, 100), c("Red", "White", "Blue"))
+#   
+#   factors <- rep(c("ts_0_1_H", 
+#                    "ts_0_1_L_a",
+#                    "ts_0_1_L_b",
+#                    "ts_1_1",
+#                    "ts_Inf_Inf_H", 
+#                    "ts_Inf_Inf_0",
+#                    "ts_0_Inf_L"),
+#                times = c(sum(subsets[["ts_0_1_H"]] %in% prob_ts), 
+#                          sum(subsets[["ts_0_1_L_a"]] %in% prob_ts),
+#                          sum(subsets[["ts_0_1_L_b"]] %in% prob_ts), 
+#                          sum(subsets[["ts_1_1"]] %in% prob_ts), 
+#                          sum(subsets[["ts_Inf_Inf_H"]] %in% prob_ts), 
+#                          sum(subsets[["ts_Inf_Inf_0"]] %in% prob_ts), 
+#                          sum(subsets[["ts_0_Inf_L"]] %in% prob_ts)))
+# 
+#   
+#   mat_list <- list(ts_0_1_H = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_H"]])],
+#                    ts_0_1_L_a = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_L_a"]])],
+#                    ts_0_1_L_b = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_1_L_b"]])],
+#                    ts_1_1 = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_1_1"]])],
+#                    ts_Inf_Inf_H = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_Inf_Inf_H"]])],
+#                    ts_Inf_Inf_0 = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_Inf_Inf_0"]])],
+#                    ts_0_Inf_L = plot_mat[ , which(colnames(plot_mat) %in% subsets[["ts_0_Inf_L"]])])
+#   
+#   dend_list <- list(ts_0_1_H = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_H"]])))),
+#                     ts_0_1_L_a = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_L_a"]])))),
+#                     ts_0_1_L_b = as.dendrogram(hclust(dist(t(mat_list[["ts_0_1_L_b"]])))),
+#                     ts_1_1 = as.dendrogram(hclust(dist(t(mat_list[["ts_1_1"]])))),
+#                     ts_Inf_Inf_H = as.dendrogram(hclust(dist(t(mat_list[["ts_Inf_Inf_H"]])))),
+#                     ts_Inf_Inf_0 = as.dendrogram(hclust(dist(t(mat_list[["ts_Inf_Inf_0"]])))),
+#                     ts_0_Inf_L = as.dendrogram(hclust(dist(t(mat_list[["ts_0_Inf_L"]])))))
+#   
+#   filename <- sprintf("outputs/test_statistics/plots/circos_%s_chapter3.pdf",
+#                       pred)
+#   
+#   pdf(filename,
+#       width = 10,
+#       height = 10)
+#   
+#   circos.par(cell.padding = c(0, 
+#                               0, 
+#                               0, 
+#                               0), 
+#              gap.degree = 10,
+#              "track.height" = 0.8)
+#   
+#   circos.initialize(factors = factors,
+#                     xlim = cbind(0, table(factors)))
+#   
+#   circos.track(ylim = c(0, 6), 
+#                bg.border = NA, 
+#                panel.fun = function(x, y){
+#                  
+#                  sector.index = CELL_META$sector.index
+#                  m = mat_list[[sector.index]]
+#                  dend = dend_list[[sector.index]]
+#                  
+#                  m2 = m[, order.dendrogram(dend)]
+#                  col_mat = col_fun(m2)
+#                  nr = nrow(m2)
+#                  nc = ncol(m2)
+#                  for(i in 1:nr) {
+#                    circos.rect(1:nc - 1, rep(nr - i, nc), 
+#                                1:nc, rep(nr - i + 1, nc), 
+#                                border = col_mat[i, ], col = col_mat[i, ])
+#                    
+#                  }
+#                  circos.axis(h = "top",
+#                              major.tick = FALSE,
+#                              labels = colnames(mat_list[[sector.index]]),
+#                              labels.cex = 0.6,
+#                              labels.facing = "bending.inside",
+#                              labels.away.percentage = 0.3)
+#                  circos.yaxis(labels = model_order_prob, 
+#                               labels.cex = 0.6)
+#                })
+#   
+#   circos.clear()
+#   
+#   dev.off()
+# }
